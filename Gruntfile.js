f3ade2554fc21189e3f86e33278ec8079e9e2387
hongbinc <hongbin1992@hotmail.com>
2016-03-03 18:04:06 -0500

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+﻿module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        responsive_images: {
+            dev: {
+                options: {
+                    engine: 'im',
+                    sizes: [{
+                        name: 'small',
+                        width: '30%',
+                        suffix: '_small',
+                        quality: 20
+                    }, {
+                        name: 'large',
+                        width: 1000,
+                        suffix: '_large',
+                        quality: 40
+                    }]
+                },
+                files: [{
+                    expand: true,
+                    src: ['*.{gif,jpg,png}'],
+                    cwd: 'images_src',
+                    dest: 'images/'
+                }]
+            }
+        }
+    });
+
+    // Load the plugin that provides the "responsive-image" task.
+    grunt.loadNpmTasks('grunt-responsive-images');
+
+    // Default task(s).
+    grunt.registerTask('default', ['responsive_images']);
+
+};
